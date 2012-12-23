3ee7aa134f17c913b891c0bd34d14ab64b6162b2
Brian Doyle <brianpdoyle@gmail.com>
2012-12-23 09:37:48 -0700

    worked out how the board determines which row has the turn and when the game is over

--- mancala.groovy
+++ mancala.groovy
@@ -1,66 +1,101 @@
 
-String player1 = null
-String potLetter = null
-String player2 = null
 Board board = null
 
 System.in.withReader { 
   print "Enter your a name for Player 1 : "
-  player1 = (it.readLine()) 
+  String player1 = (it.readLine()) ?: "Player 1"
 
   print "Enter your a name for Player 2 : "
-  player2 = (it.readLine()) 
+  String player2 = (it.readLine()) ?: "Player 2"
 
   board = new Board(player1, player2)
 
+  println ""
   println board.toString()
+  println ""
+  Row rowTurn = board.nextRowTurn()
 
-  while (!board.isGameOver()) {
-    print "Enter the pot [A B C D E F]: "
-    potLetter = it.readLine()
-    println "You entered the letter: ${letter}"
+  while (rowTurn) {
+    println "${rowTurn}"
+    print "Enter the pot: ${rowTurn.validPots()}: "
+    pot = it.readLine() as char 
+    if (rowTurn.isValidPot(pot)) {
+      println "You entered the letter: ${pot}"
+      board.playTurn(pot)
+      println ""
+      println board.toString()
+      println ""
+    }
+    else {
+      println "Invalid pot ${pot}, try again."
+    }
+
+    rowTurn = board.nextRowTurn()
   }
 }
 
 println ""
 println "The game is over."
-println "The winner is ${board.winner()}"
+println "The winner is ${board.winnerRow()}"
 
 class Board {
-  Row topRow
-  Row bottomRow
+  Collection<Row> rows = []
+  int currentRowIdx = 1
 
   public Board(String name1, String name2) {
-    topRow = new Row(name1)
-    bottomRow = new Row(name2)
+    rows = [new Row(name2, false), new Row(name1, true)]
   }
 
   public String toString() {
-    return "\nTop Row: ${topRow}\n\nBottom Row: ${bottomRow}\n"
+    return "${rows.join('\n')}"
   }
 
-  public boolean isGameOver() {
-    return true
+  public Row winnerRow() {
+    if (!rows.every { it.hasAnyRemainingTurns()} ) {
+      return rows[0].bank > rows[1].bank ? rows[0] : rows[1]
+    }
+    else {
+      return null
+    }
   }
 
-  public Row winner() {
-    return topRow
+  public Row nextRowTurn() {
+    Row winner = winnerRow()
+    if (winner) {
+      return null
+    }
+    else {
+      currentRow()
+    }
+  }
+
+  public void playTurn(char pot) {
+    if (!currentRow().moveBeansFromPot(pot)) {
+      currentRowIdx = currentRowIdx == 0 ? 1 : 0
+    }
+  }
+
+  public Row currentRow() {
+    return rows[currentRowIdx]
   }
 
 }
 
 class Row { 
   int bank = 0
-  Map pots = [:]
+  LinkedHashMap pots = [:]
   String userName
+  int turnsTaken = 0 
+  boolean topRow = false
 
-  public Row(String userName) {
+  public Row(String userName, topRow) {
     this.userName = userName
+    this.topRow = topRow
     
     int potsPerSide = 6
-    int aChar = 65
+    int aChar = 97
 
-    (0..5).each { pot ->
+    (0..(potsPerSide - 1)).each { pot ->
       char letter = (pot + aChar) as char
       pots[letter] = 4
     }
@@ -68,8 +103,28 @@ class Row {
     bank = 0
   }
 
+  public Collection validPots() {
+    return pots.findAll { pot, beadCount ->
+      return beadCount > 0
+    }.keySet()
+  }
+
   public String toString() {
-    return "Name: ${userName}, Bank: ${bank}, Pots: ${pots}"
+    return "TopRow: ${topRow}, Name: ${userName}, Bank: ${bank}, Pots: ${pots}"
+  }
+
+  public boolean isValidPot(char pot) {
+    return validPots().contains(pot)
+  }
+
+  public boolean hasAnyRemainingTurns() {
+    return turnsTaken == 0
+  }
+
+  public boolean moveBeansFromPot(char pot) {
+    turnsTaken += 1
+    println "Turns taken ${turnsTaken}"
+    return false
   }
 
 }
